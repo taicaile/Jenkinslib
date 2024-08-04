439e34d35d2d83534a4ba9e6570273649c21254c
taicaile <23215696+taicaile@users.noreply.github.com>
2024-08-04 23:26:24 +0800

    remove read yaml

--- myLocalLib.groovy
+++ myLocalLib.groovy
@@ -1,16 +1,16 @@
 // myLocalLib.groovy
-import groovy.yaml.YamlSlurper
+// import groovy.yaml.YamlSlurper
 
 def myFunction() {
     echo 'This is a function from my local Groovy library.'
 }
 
-def readYamlFile(String filename) {
-    if (new File(filename).exists()) {
-        return new YamlSlurper().parse(new File(filename))
-    } else {
-        logger.error("Config file not found: $filename")
-    }
-}
+// def readYamlFile(String filename) {
+//     if (new File(filename).exists()) {
+//         return new YamlSlurper().parse(new File(filename))
+//     } else {
+//         logger.error("Config file not found: $filename")
+//     }
+// }
 
 return this
